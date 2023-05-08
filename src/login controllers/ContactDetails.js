8db25f203f245db2e5231ae9e023ec4cbca0e855
maneesh yadav <maneesh.yadav@cylsys.com>
2023-05-08 14:16:52 +0530

    contactupdateprocessor

--- src/login controllers/ContactDetails.js
+++ src/login controllers/ContactDetails.js
@@ -14,7 +14,8 @@ exports.ContactDetails = async (req, res, next) => {
       });
     }
     const [rows] = await dbConn.execute(
-      "insert into tbl_contactdetails (`firstName`,`lastName`,`contact_Email`,`user_Details`,`file_UploadId`,`IsActive`,`list_Id`,`company_Id`,`contact_Number`) values(?,?,?,?,?,?,?,?,?)",
+      // "insert into tbl_contactdetails (`firstName`,`lastName`,`contact_Email`,`user_Details`,`file_UploadId`,`IsActive`,`list_Id`,`company_Id`,`contact_Number`) values(?,?,?,?,?,?,?,?,?)",
+      'call sendquickmail_db.Contactdetails(?,?,?,?,?,?,?,?,?)',
       [
         req.body.firstName,
         req.body.lastName,
@@ -58,16 +59,17 @@ exports.ContactDetailsEdit = async (req, res, next) => {
       });
     }
     const [rows1] = await dbConn.execute(
-      "UPDATE tbl_contactdetails SET `firstName` =?,`company_Id`=?,`lastName`=?,`contact_Email`=?,`user_Details`=?,`file_UploadId`=?,`IsActive`=?,`list_Id`=?,`contact_Number`=? WHERE `contact_id` = ?",
+      // "UPDATE tbl_contactdetails SET `firstName` =?,`company_Id`=?,`lastName`=?,`contact_Email`=?,`user_Details`=?,`file_UploadId`=?,`IsActive`=?,`list_Id`=?,`contact_Number`=? WHERE `contact_id` = ?",
+      'call sendquickmail_db.Update_ContactDetails(?,?,?,?,?,?,?,?,?,?)',
       [
         req.body.firstName,
-        req.body.company_Id,
         req.body.lastName,
         req.body.contact_Email,
         req.body.user_Details,
         req.body.file_UploadId,
         req.body.IsActive,
         req.body.list_Id,
+        req.body.company_Id,
         req.body.contact_Number,
         req.body.contact_id,
       ]
@@ -103,9 +105,23 @@ exports.ContactUnSubscribe = async (req, res, next) => {
         message: "Invalid contact_Email",
       });
     }
+
+    console.log('....................', req?.body?.isActive);
+    let checkValue = '';
+    if (typeof req?.body?.isActive === 'boolean') {
+      if (req?.body?.isActive == true) checkValue = '1';
+      else checkValue = '0';
+      console.log("checkValue_...", checkValue);
+    } else {
+      throw { message: 'isActive should be boolena' }
+    }
+
+    console.log('jbb bib jbi000000000',typeof checkValue);
     const [rows1] = await dbConn.execute(
-      "UPDATE tbl_contactdetails SET `IsActive`=?  WHERE `contact_Email` = ?",
-      [req.body.IsActive, req.body.contact_Email]
+      // "UPDATE tbl_contactdetails SET `IsActive`=?  WHERE `contact_Email` = ?",
+      'call sendquickmail_db.Unsubscribe(?,?)',
+      [String(checkValue), req.body.contact_Email]
+      // ['0', req.body.contact_Email]
     );
     if (rows1.affectedRows === 1) {
       return res.status(201).json({
