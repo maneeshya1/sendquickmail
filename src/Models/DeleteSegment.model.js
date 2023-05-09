43ada1b22ce992934e8e28b0c628a93d22cd16f0
maneesh yadav <maneesh.yadav@cylsys.com>
2023-05-09 13:27:29 +0530

    deleteSegment

--- src/Models/DeleteSegment.model.js
+++ src/Models/DeleteSegment.model.js
@@ -2,7 +2,7 @@
 var dbConn = require('./../../config/db.config');
 
 exports.delete = function (segment_Id, result) {
-    dbConn.query("DELETE FROM tbl_segment WHERE segment_Id = ?", [segment_Id], function (err, res) {
+    dbConn.query('call sendquickmail_db.Delete_Segment(?)', [segment_Id], function (err, res) {
         if (err) {
             console.log("error: ", err);
             result(null, err);
