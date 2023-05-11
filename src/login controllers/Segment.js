af20ad8d2fbe67992042d32b5f82bceae0e1a894
maneesh yadav <maneesh.yadav@cylsys.com>
2023-05-11 17:59:10 +0530

    updatesegment

--- src/login controllers/Segment.js
+++ src/login controllers/Segment.js
@@ -108,8 +108,8 @@ exports.UpdateSegment = async (req, res, next) => {
 
     const [rows] = await dbConn.execute(
       
-      // "UPDATE tbl_segment SET `segmentName` = ?, `criteria` = ?, `contactfieldType` = ?, `FieldfindBy` = ?, `Is_And` = ?, `Is_Or` = ?, `contains` = ?,`segment_users`= ?, `IsActive` = ? WHERE `segment_Id` = ?",
-      'call sendquickmail_db.Update_Segment(?,?,?,?,?,?,?,?,?,?,?,?)',
+      "UPDATE tbl_segment SET `segmentName` = ?, `criteria` = ?, `contactfieldType` = ?, `FieldfindBy` = ?, `Is_And` = ?, `Is_Or` = ?, `contains` = ?,`segment_users`= ?, `IsActive` = ? ,`UserId` = ?,`company_Id` = ? WHERE `segment_Id` = ?",
+      // 'call sendquickmail_db.Update_Segment(?,?,?,?,?,?,?,?,?,?,?,?)',
 
       [
         req.body.segmentName,
