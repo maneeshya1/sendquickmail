33ea60f68f75d4d64da969f79c48a2f387c2999c
maneesh yadav <maneesh.yadav@cylsys.com>
2023-05-13 13:52:57 +0530

    contact

--- src/login controllers/GetContactDetails.js
+++ src/login controllers/GetContactDetails.js
@@ -1,26 +1,5 @@
 const dbConn = require('./../../config/db.config').promise();
 
-// exports.GetContactDetails = (req, res) => {
-//         dbConn.query('SELECT `user_Details`,`contact_Email` FROM `tbl_contactdetails` WHERE `contact_Email`= ?', [req.params.contact_Email], (err, rows, fields) => {
-//                 if (!err)
-//                         res.send(rows);
-//                 else {
-//                         res.send(err);
-//                         console.log("contact not exist");
-//                 }
-//         })
-// };
-
-// exports.GetContactDetails = (req, res) => {
-//         dbConn.query('SELECT * FROM tbl_contactdetails ', (err, rows, fields) => {
-//                 if (!err)
-//                         res.send(rows);
-//                 else {
-//                         res.send(err);
-//                         console.log("user not exist");
-//                 }
-//         })
-// };
 
 exports.GetContactDetails = async (req, res, next) => {
         try {
