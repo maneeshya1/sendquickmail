df7f5a77f87a8edb80ad1685b021cfb59cfb5a24
maneesh yadav <maneesh.yadav@cylsys.com>
2023-07-10 16:42:01 +0530

    SearchApi

--- src/Controller/NewGetSegment.js
+++ src/Controller/NewGetSegment.js
@@ -1,10 +1,22 @@
 const Segment = require('../Models/NewGetSegment');
-
-exports.findBySearch = function(req, res) {
-    console.log('.................',req.body);
-    Segment.findBySearch(req.body, function(err, Segment) {
-        if (err)
-        res.send(err);
-        res.json(Segment);
-    });
-};
+exports.findBySearch = async (req, res) => {
+    const bodyKeys = Object.keys(req.body);
+    var users = '';
+    try {
+        if(bodyKeys[0] == 'first_Name'){
+            const { first_Name } = req.body;
+            users = await Segment.searchUsersByFirstName(first_Name);
+        } else if(bodyKeys[0] == 'email'){ console.log('testt');
+            const { email } = req.body;
+            console.log('payload', email);
+            users = await Segment.searchUsersByEmail(email);
+        } else if(bodyKeys[0] == 'last_name'){
+            const { last_name } = req.body;
+            users = await Segment.searchUsersByLastName(last_name);
+        }
+        res.json({ users });
+    } catch (error) {
+      console.error('Error searching users:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
--- src/Models/NewGetSegment.js
+++ src/Models/NewGetSegment.js
@@ -1,84 +1,55 @@
 var dbConn = require('./../../config/db.config');
-
-//TimeSheet object create
-var Segment = function (Segmentdata) {
-
-    this.contact_id = Segmentdata.contact_id;
-    this.firstName = Segmentdata.firstName;
-    this.lastName = Segmentdata.lastName;
-    this.contact_Email = Segmentdata.contact_Email;
-    this.contact_Number = Segmentdata.contact_Number;const { log } = require('util');
-    const dbConn = require('./../../config/db.config');
-    
-    // Segment object constructor
-    const Segment = function (Segmentdata) {
-        this.contact_id = Segmentdata.contact_id;
-        this.first_Name = Segmentdata.first_Name;
-        this.lastName = Segmentdata.lastName;
-        this.contact_Email = Segmentdata.contact_Email;
-        this.contact_Number = Segmentdata.contact_Number;
-        this.company_Id = Segmentdata.company_Id;
-    };
-    
-    Segment.findBySearch = function (params) {
-        const param_key = Object.keys(params)[0]; // Assuming only one key in params object
-        let param_value = '';
-        console.log("Mani",param_key);
-        if (param_key === 'contact_Email') {
-            param_value = params.contact_Email;
-            const sql = 'CALL GetUserByEmail(?)';
-    
-            return new Promise((resolve, reject) => {
-                dbConn.query(sql, param_value, (error, results) => {
-                    if (error) {
-                        reject(error);
-                        return;
-                    }
-                    console.log("Jashi",results);
-                    resolve(results);
-                });
-            });
-        } else if (param_key === 'first_Name') {
-            param_value = params.first_Name; // corrected parameter key
-            const sql = 'call GetUserByFirstName(?)'
-            return new Promise((resolve, reject) => {
-                dbConn.query(sql, param_value, (error, results) => {
-                    console.log("Akhi",sql);
-                    if (error) {
-                        reject(error);
-                        return;
-                    }
-                    console.log("Honey",results);
-                    resolve(results);
-                });
-            });
-        } else {
-            return Promise.reject('Invalid search parameter');
-        }
-    };
-    
-    module.exports = Segment;
-    
-
-    // this.Task = Segmentdata.Task;
-    this.company_Id = Segmentdata.company_Id;
-};
-
-Segment.findBySearch = function (params, result) {
-    let firstName = params.firstName;
-    let lastName = params.lastName;
-    let contact_Email = params.contact_Email;
-    var sql = 'SELECT * FROM tbl_contactdetails WHERE firstName = ? OR lastName = ? OR contact_Email = ?';
-    dbConn.query(sql, [firstName, lastName, contact_Email
-    ], function (err, res) {
-        if (err) {
-            console.log("error: ", err);
-            result(err, null);
-        }
-        else {
-            result(null, res);
-        }
+exports.searchUsersByFirstName = (searchTerm) => {
+    return new Promise((resolve, reject) => {
+        dbConn.query(
+            'CALL GetUserByFirstName(?)',
+            [searchTerm],
+            (err, results) => {
+                if (err) {
+                    console.error('Error executing stored procedure:', err);
+                    reject(err);
+                    return;
+                }
+                const users = results[0];
+                resolve(users);
+            }
+        );
     });
 };
 
-module.exports = Segment;
+exports.searchUsersByEmail = (search_term) => {
+    console.log('test email', search_term);
+    return new Promise((resolve, reject) => {
+        dbConn.query(
+            'CALL GetUserByEmail(?)',
+            [search_term],
+            (err, results) => {
+                if (err) {
+                    console.error('Error executing stored procedure:', err);
+                    reject(err);
+                    return;
+                }
+                const users = results[0];
+                resolve(results);
+            }
+        );
+    });
+};
+
+exports.searchUsersByLastName = (searchTerm) => {
+    return new Promise((resolve, reject) => {
+        dbConn.query(
+            'CALL GetUserBylastName(?)',
+            [searchTerm],
+            (err, results) => {
+                if (err) {
+                    console.error('Error executing stored procedure:', err);
+                    reject(err);
+                    return;
+                }
+                const users = results[0];
+                resolve(users);
+            }
+        );
+    });
+};
